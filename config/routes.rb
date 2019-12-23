a7c618ade223bf9405bc59e6ab5b1661cb6ad2ac
Uzay-G <uzgirit@gmail.com>
2019-12-23 23:10:43 +0100

    landing page in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root :to => "application#home"
+  get  '/signup',  to: 'users#new'
 end
