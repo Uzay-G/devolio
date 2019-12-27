1d939f0c0c894871ee984a78310a671a960f5dbc
Uzay-G <uzgirit@gmail.com>
2019-12-28 00:16:38 +0100

    remove force_ssl

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,16 @@ Rails.application.routes.draw do
   #resources :user_sessions
   resources :contacts
 
-  #resources :users do
-   # member do
-   #   get :activate
-   # end
- # end
+  resources :users do
+    member do
+      get :activate
+    end
+  end
   
- # get '/login', to: 'user_sessions#new', :as => :login
- # post '/logout' => 'user_sessions#destroy', :as => :logout
+  get '/login', to: 'user_sessions#new', :as => :login
+  post '/logout' => 'user_sessions#destroy', :as => :logout
   root :to => "application#home"
   get "/discuss", to: "application#discuss"
- # get  '/signup',  to: 'users#new'
+  get  '/signup',  to: 'users#new'
   resources :user_zeros, only: [:new, :create]
 end
