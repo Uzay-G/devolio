09d3cd1195f6b644cddfb0bd61e82075eecb779d
Uzay-G <uzgirit@gmail.com>
2019-12-27 20:42:40 +0100

    make it better

--- app/controllers/user_zeros_controller.rb
+++ app/controllers/user_zeros_controller.rb
@@ -7,13 +7,13 @@ class UserZerosController < ApplicationController
     @user_zero = UserZero.new(user_params)
     respond_to do |format|
       if @user_zero.save
-        flash[:success] = "You were successfully subscribed! We'll send you updates and we hope you'll like our platform."
+        flash[:notice] = "You were successfully subscribed! We'll send you updates and we hope you'll like our platform."
         UserZeroMailer.thanks(@user_zero).deliver_now
         format.html do
           redirect_to "/discuss"
         end
       else
-        format.html { render "application/home" }
+        format.html { render :new }
         format.json { render json: @user_zero.errors, status: :unprocessable_entity }
       end
    # if @user.save
