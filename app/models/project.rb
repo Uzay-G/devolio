edb0f2ac7df593ca796fe34045c6f241cfa4163c
Uzay-G <uzgirit@gmail.com>
2020-02-11 12:46:45 +0100

    enhancing project ui

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,7 @@ class Project < ApplicationRecord
 
   belongs_to :user
 
+  validates :description, length: { maximum: 70 }
   validates :user_id, presence: true
   validates :url, format: { with: URI.regexp }, allow_blank: true
 end
