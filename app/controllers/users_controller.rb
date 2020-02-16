8ffa0e68f256921819698b0b647eaa12ae014774
Uzay-G <uzgirit@gmail.com>
2020-02-16 22:55:31 +0100

    refinining user avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -83,14 +83,14 @@ class UsersController < ApplicationController
     @title = "Following"
     @user  = User.find(params[:id])
     @users = @user.following.paginate(page: params[:page])
-    render 'show_follow'
+    render 'relationships/show_follow'
   end
 
   def followers
     @title = "Followers"
     @user  = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
-    render 'show_follow'
+    render 'relationships/show_follow'
   end
 
   private
