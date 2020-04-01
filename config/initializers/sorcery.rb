4f3cf7ec40eb2f10b0ac9b99f7a365280cba02cc
Uzay-G <uzgirit@gmail.com>
2020-04-01 14:29:23 +0200

    scope fix

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -84,7 +84,7 @@ Rails.application.config.sorcery.configure do |config|
   config.github.key = ENV["github_id"]
   config.github.secret = ENV["github_secret"]
   config.github.callback_url = "https://www.devol.io/oauth/callback?provider=github"
-  config.github.scope = "read:user"
+  config.github.scope = "read:user%20user:email"
   config.github.user_info_mapping = {:username => "name", :email => "email", :github => "name" }
   # You can change it by your local ca_file. i.e. '/etc/pki/tls/certs/ca-bundle.crt'
   # Path to ca_file. By default use a internal ca-bundle.crt.
